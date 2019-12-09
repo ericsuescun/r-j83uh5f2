06efa3cb0052a106947d846c03cb1d3ce721ae52
Eric Suescun <edsuescun@gmail.com>
2019-12-09 16:26:11 -0500

    Se implementa  llave cont dentro del state. y esta es actualizada por el evento e, que se atrapa con la funcin update.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+	constructor() {
+		super();
+		this.state = { count: 0 }
+	}
+
+	update(e) {
+		this.setState({count: e.target.value.length})
+	}
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.update.bind(this)}></textarea>
+        <div className="counter">{this.state.count}</div>
       </div>
     );
   }
